5bbb5a24b484c40069d11b37cd031e809378228e
ckw <2437037469@qq.com>
2020-09-03 12:33:30 +0800

    feat: vue源码：在渲染VNode的过程中触发hook，设置DOOM的相关属性

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-typescript';
 import replace from 'rollup-plugin-replace';
-const name = 'vdom'
+const name = 'vdom-with-hook'
 const generateVueFilepath = name => `./src/vue/${name}/example/`
 export default {
   input: generateVueFilepath(name) + 'index.ts',
---
+++ src/vue/vdom-with-hook/vdom/createElement.ts
@@ -0,0 +1,62 @@
+import { VNodeData, VNodeChildren } from '../types/vnode';
+import { isPrimitive, isDef } from '../utils/is';
+import { createTextVNode, VNode, createEmptyVNode } from './vnode';
+export function createElement(
+  tag?: string,
+  data?: VNodeData,
+  children?: VNodeChildren
+): VNode {
+  /* 没有传入data */
+  if (Array.isArray(data) || isPrimitive(data)) {
+    children = data
+    data = undefined
+  }
+
+  if (!tag) return createTextVNode()
+
+  children = normalizeChildren(children) as VNode[]
+
+  let vnode
+  if (typeof tag === 'string') {
+    vnode = new VNode({
+      tag,
+      data,
+      children
+    })
+  } else {
+    // TODO component
+  }
+
+  if (isDef(vnode)) {
+    return vnode
+  } else {
+    return createEmptyVNode()
+  }
+}
+
+/**
+ * 校验子组件是否符合规范
+ */
+function normalizeChildren(children: VNodeChildren) {
+  return isPrimitive(children)
+    ? [ createTextVNode(children) ]
+    : Array.isArray(children)
+      ? normalizeArrayChildren(children)
+      : undefined
+}
+/**
+ * TODO 省略了合并相邻文本节点的过程
+ */
+function normalizeArrayChildren(children: VNodeChildren[], nestedIndex: string = '') {
+  return children.map((child, i) => {
+    if (!isDef(child) || typeof child === 'boolean') return null
+    if (isPrimitive(child)) {
+      return createTextVNode(child)
+    } else if (Array.isArray(child)) {
+      return normalizeArrayChildren(child, `${nestedIndex}_${i}`)
+    } else {
+      // TODO 如果是v-for的情况
+      return child
+    }
+  })
+}
